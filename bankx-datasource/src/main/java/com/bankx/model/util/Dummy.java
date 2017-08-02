7f65a55139818781279091cbcb2b376a2a2b93e6
Jennya Dobreva <jennya.dobreva@estafet.com>
2017-08-02 11:44:57 +0300

    Current state persistence modules

---
+++ bankx-datasource/src/main/java/com/bankx/model/util/Dummy.java
@@ -0,0 +1,9 @@
+package com.bankx.model.util;
+
+public class Dummy {
+
+	public Dummy() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ bankx-models/src/main/java/com/estafet/fuse/model/Account.java
@@ -0,0 +1,107 @@
+package com.estafet.fuse.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name="accounts")
+public class Account implements Serializable {
+	
+	private static final long serialVersionUID = 8509433648225046161L;
+	
+	@Id
+	private String iban;
+	@Column
+	private String name;
+	@Column
+	private double balance;
+	@Column
+	private String currency;
+	
+	
+	public Account(String iban, String name, double balance, String currency) {
+		super();
+		this.iban = iban;
+		this.name = name;
+		this.balance = balance;
+		this.currency = currency;
+	}
+	
+	public Account(String iban) {
+		super();
+		this.iban = iban;
+	}
+
+	public String getIban() {
+		return iban;
+	}
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(balance);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((currency == null) ? 0 : currency.hashCode());
+		result = prime * result + ((iban == null) ? 0 : iban.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (Double.doubleToLongBits(balance) != Double.doubleToLongBits(other.balance))
+			return false;
+		if (currency == null) {
+			if (other.currency != null)
+				return false;
+		} else if (!currency.equals(other.currency))
+			return false;
+		if (iban == null) {
+			if (other.iban != null)
+				return false;
+		} else if (!iban.equals(other.iban))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
